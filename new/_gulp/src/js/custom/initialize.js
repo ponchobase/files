67a42f4fda1249775b5a0587c921e58dd07d7445
ponchobase <ponchobase@gmail.com>
2024-09-30 23:55:04 -0400

    update

---
+++ new/_gulp/src/js/custom/initialize.js
@@ -0,0 +1,14 @@
+try {
+    // Initialize app
+    initialize_app();
+} catch (e) {
+    // console.error(e);
+}
+
+function initialize_app() {
+    try {
+        console.log("Poncho on BASE");
+    } catch (e) {
+        // console.error(e);
+    }
+}
