44c5e227d4de37f400515ba36891d841b9a24fef
ponchobase <ponchobase@gmail.com>
2024-10-03 13:54:59 -0400

    update

--- new/_gulp/src/js/custom/_common/swipers.js
+++ new/_gulp/src/js/custom/_common/swipers.js
@@ -1,5 +1,8 @@
 function init_swipers() {
     try {
+        // Init about swiper
+        init_about_swiper();
+
         // Init roadmap swiper
         init_roadmap_swiper();
     } catch (e) {
@@ -7,16 +10,14 @@ function init_swipers() {
     }
 }
 
-function init_roadmap_swiper() {
+function init_about_swiper() {
     try {
         // Define vars
-        var selector_roadmap_swiper = ".roadmap-swiper";
-
-        // Define vars
-        var data_roadmap_swiper = "[data-swiper=roadmap-swiper]";
+        var selector_swiper = ".about-swiper";
+        var data_swiper = "[data-swiper=about-swiper]";
 
         // Init new swiper
-        new Swiper(data_roadmap_swiper, {
+        new Swiper(data_swiper, {
             // Optional parameters
             direction: "horizontal",
             freeMode: true,
@@ -29,8 +30,41 @@ function init_roadmap_swiper() {
                 forceToAxis: true
             },
             navigation: {
-                nextEl: selector_roadmap_swiper + " .swiper-next",
-                prevEl: selector_roadmap_swiper + " .swiper-prev"
+                nextEl: selector_swiper + " .swiper-next",
+                prevEl: selector_swiper + " .swiper-prev"
+            },
+            slidesPerView: "auto",
+            slidesPerGroup: 1,
+            spaceBetween: 16,
+            updateOnWindowResize: true
+        });
+    } catch (e) {
+        // console.error(e);
+    }
+}
+
+function init_roadmap_swiper() {
+    try {
+        // Define vars
+        var selector_swiper = ".roadmap-swiper";
+        var data_swiper = "[data-swiper=roadmap-swiper]";
+
+        // Init new swiper
+        new Swiper(data_swiper, {
+            // Optional parameters
+            direction: "horizontal",
+            freeMode: true,
+            keyboard: {
+                enabled: true,
+                onlyInViewport: false,
+            },
+            loop: true,
+            mousewheel: {
+                forceToAxis: true
+            },
+            navigation: {
+                nextEl: selector_swiper + " .swiper-next",
+                prevEl: selector_swiper + " .swiper-prev"
             },
             slidesPerView: "auto",
             slidesPerGroup: 1,
